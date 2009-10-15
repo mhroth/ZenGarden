1d8a885f532687148ba202cdeca9f9b5b82eda76
Martin Roth <mhroth@rjdj.me>
2009-10-15 14:53:07 +0100

    remove list_c.py

--- src/list_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-for root, dirs, files in os.walk('./'):
- for name in files:
-   if name.endswith(".cpp"):
-     filename = os.path.join(root, name)
-     print filename + " \\"
