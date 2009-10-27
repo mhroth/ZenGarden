7d4e2d8e74a4b35fb0c5c08ad9e15980d195300a
Martin Roth <mhroth@rjdj.me>
2009-10-27 14:21:03 +0000

    Add LGPL license header to all Dsp files.

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char * const argv[]) {
   const int sampleRate = 22050;
   const float floatHalfRandMax = (float) (RAND_MAX >> 1);
   
-  PdGraph *graph = PdGraph::newInstance("/Users/mhroth/Desktop/Echolon.rj/", "_main.pd", "/Users/mhroth/rjdj/client/trunk/extradata/rjlib/",
+  PdGraph *graph = PdGraph::newInstance("/Users/mhroth/Desktop/", "64_osc.pd", "/Users/mhroth/rjdj/client/trunk/extradata/rjlib/",
                                         blockSize, numInputChannels, numOutputChannels, sampleRate);
   if (graph == NULL) {
     printf("PdGraph could not be created. Is the filename correct?\n");
@@ -22,6 +22,7 @@ int main(int argc, char * const argv[]) {
   
   float audioInput[blockSize * numInputChannels];
   float audioOutput[blockSize * numOutputChannels];
+  /*
   char *objectInitString = StaticUtils::copyString("send_external #accelerate");
   MessageExternalSend *sendAccelerate = new MessageExternalSend("#accelerate", objectInitString);
   free(objectInitString);
@@ -31,14 +32,14 @@ int main(int argc, char * const argv[]) {
   accelerateMessage->addElement(new MessageElement(0.0f));
   accelerateMessage->addElement(new MessageElement(-9.8f));
   accelerateMessage->setBlockIndex(0);
-  
+  */
   graph->prepareForProcessing();
   for (int z = 0; z < 1000000; z++) {
    // pass in noise to the adc~s
    for (int i = 0; i < blockSize * numInputChannels; i++) {
      audioInput[i] = ((float) rand() / floatHalfRandMax) - 1.0f;
    }
-   sendAccelerate->setExternalMessage(accelerateMessage);
+   //sendAccelerate->setExternalMessage(accelerateMessage);
    graph->process(audioInput, audioOutput);
   /*
    for (int i = 0; i < blockSize; i++) {
@@ -52,7 +53,7 @@ int main(int argc, char * const argv[]) {
   
   free(audioInput);
   free(audioOutput);
-  delete accelerateMessage;
+  //delete accelerateMessage;
   
   return 0;
 }
