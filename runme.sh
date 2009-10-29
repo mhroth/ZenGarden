9f88688db87c6b2cc35d0abda551a15653d22eb8
chrism <chrism@byzo.local>
2009-10-29 11:26:09 +0000

    Gave runme.sh an optional argument. README and TODO list tweaks. Removed obsolete libs/osx/ directory.

--- runme.sh
+++ runme.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-java -Djava.library.path=./libs/`./src/platform`/ -jar ZenGarden.jar pd-patches/simple_osc.pd
+
+if [ "$1" == "" ]
+then
+	patch=pd-patches/simple_osc.pd
+else
+	patch=$1
+fi
+
+java -Djava.library.path=./libs/`./src/platform`/ -jar ZenGarden.jar $patch
