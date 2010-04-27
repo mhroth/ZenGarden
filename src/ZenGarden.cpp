ea36acbc0839d33390228cd1f5181ad0fe65d4c9
Martin Roth <mhroth@rjdj.me>
2010-04-27 11:32:33 +0100

    First commit of ZenGarden.h/cpp. This is the ZenGarden header which should be added with the library in order to integrate it into a project.

---
+++ src/ZenGarden.cpp
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2010 Reality Jockey, Ltd.
+ *                 info@rjdj.me
+ *                 http://rjdj.me/
+ * 
+ *  This file is part of ZenGarden.
+ *
+ *  ZenGarden is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ZenGarden is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with ZenGarden.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "PdGraph.h"
+#include "ZenGarden.h"
+
+ZGGraph *zg_new_graph(char *directory, char *filename, int blockSize, 
+                      int numInputChannels, int numOutputChannels, float sampleRate) {
+  return PdGraph::newInstance(directory, filename, NULL, blockSize, 
+                              numInputChannels, numOutputChannels, sampleRate, NULL);
+}
+
+void zg_delete_graph(PdGraph *graph) {
+  delete graph;
+}
+
+void zg_process(PdGraph *graph, float *inputBuffers, float *outputBuffers) {
+  graph->process(inputBuffers, outputBuffers);
+}
