1709d7b7b855e21b7fda7ce1791b4914d1336d3b
Martin Roth <mhroth@rjdj.me>
2010-06-04 21:45:06 +0100

    Add a central implementation of all ArrayArithmetic operations.

---
+++ src/ArrayArithmetic.h
@@ -0,0 +1,163 @@
+/*
+ *  Copyright 2010 Reality Jockey, Ltd.
+ *                 info@rjdj.me
+ *                 http://rjdj.me/
+ * 
+ *  This file is part of ZenGarden.
+ *
+ *  ZenGarden is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ZenGarden is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with ZenGarden.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _ARRAY_ARITHMETIC_H_
+#define _ARRAY_ARITHMETIC_H_
+
+#ifdef _ARM_ARCH_7
+#include <arm_neon.h>
+#endif // _ARM_ARCH_7
+
+/**
+ * This class offers static inline functions for computing basic arithmetic with float arrays.
+ * It offers a central place for optimised implements of certain compute-intensive operations.
+ */
+class ArrayArithmetic {
+  
+  public:
+    static inline void add(float *input0, float *input1, float *output, int startIndex, int endIndex) {
+      #ifdef _ARM_ARCH_7
+      const int numFours = (endIndex - startIndex) >> 2;
+      for (int i = startIndex, j = 0; j < numFours; i+=4, j++) {
+        float32x4_t inVec0 = vld1q_f32((const float32_t *) (input0 + i));
+        float32x4_t inVec1 = vld1q_f32((const float32_t *) (input1 + i));
+        float32x4_t res = vaddq_f32(inVec0, inVec1);
+        vst1q_f32((float32_t *) (output + i), res);
+      }
+      for (int i = startIndex + numFours<<2; i < endIndex; i++) {
+        output[i] = input0[i] + input1[i];
+      }
+      #else
+      for (int i = startIndex; i < endIndex; i++) {
+        output[i] = input0[i] + input1[i];
+      }
+      #endif
+    }
+  
+    static inline void add(float *input, float constant, float *output, int startIndex, int endIndex) {
+      #ifdef _ARM_ARCH_7
+      const int numFours = (endIndex - startIndex) >> 2;
+      const float32x4_t constVec = vdupq_n_f32(constant);
+      for (int i = startIndex, j = 0; j < numFours; i+=4, j++) {
+        float32x4_t inVec = vld1q_f32((const float32_t *) (input + i));
+        float32x4_t res = vaddq_f32(inVec, constVec);
+        vst1q_f32((float32_t *) (output + i), res);
+      }
+      for (int i = startIndex + numFours<<2; i < endIndex; i++) {
+        output[i] = input[i] + constant;
+      }
+      #else
+      for (int i = startIndex; i < endIndex; i++) {
+        output[i] = input[i] + constant;
+      }
+      #endif
+    }
+    
+    static inline void subtract(float *input0, float *input1, float *output, int startIndex, int endIndex) {
+      #ifdef _ARM_ARCH_7
+      // the number of sets of four samples in the block to be processed
+      const int numFours = (endIndex - startIndex) >> 2;
+      for (int i = startIndex, j = 0; j < numFours; i+=4, j++) {
+        float32x4_t inVec0 = vld1q_f32((const float32_t *) (input0 + i)); // use VLD1 as data is NOT interleaved
+        float32x4_t inVec1 = vld1q_f32((const float32_t *) (input1 + i)); // load
+        float32x4_t res = vsubq_f32(inVec0, inVec1); // compute
+        vst1q_f32((float32_t *) (output + i), res); // store
+      }
+      // compute the remainder of the block (if any)
+      for (int i = startIndex + numFours<<2; i < endIndex; i++) {
+        output[i] = input0[i] - input1[i];
+      }
+      #else
+      for (int i = startIndex; i < endIndex; i++) {
+        output[i] = input0[i] - input1[i];
+      }
+      #endif
+    }
+  
+    static inline void subtract(float *input, float constant, float *output, int startIndex, int endIndex) {
+      #ifdef _ARM_ARCH_7
+      const int numFours = (endIndex - startIndex) >> 2;
+      const float32x4_t constVec = vdupq_n_f32(constant);
+      for (int i = startIndex, j = 0; j < numFours; i+=4, j++) {
+        float32x4_t inVec = vld1q_f32((const float32_t *) (input + i));
+        float32x4_t res = vsubq_f32(inVec, constVec);
+        vst1q_f32((float32_t *) (output + i), res);
+      }
+      for (int i = startIndex + numFours<<2; i < endIndex; i++) {
+        output[i] = input[i] - constant;
+      }
+      #else
+      for (int i = startIndex; i < endIndex; i++) {
+        output[i] = input[i] - constant;
+      }
+      #endif
+    }
+    
+    static inline void multiply(float *input0, float *input1, float *output, int startIndex, int endIndex) {
+      #ifdef _ARM_ARCH_7
+      // the number of sets of four samples in the block to be processed
+      const int numFours = (endIndex - startIndex) >> 2;
+      for (int i = startIndex, j = 0; j < numFours; i+=4, j++) {
+        float32x4_t inVec0 = vld1q_f32((const float32_t *) (input0 + i)); // use VLD1 as data is NOT interleaved
+        float32x4_t inVec1 = vld1q_f32((const float32_t *) (input1 + i)); // load
+        float32x4_t res = vmulq_f32(inVec0, inVec1); // compute
+        vst1q_f32((float32_t *) (output + i), res); // store
+      }
+      // compute the remainder of the block (if any)
+      for (int i = startIndex + numFours<<2; i < endIndex; i++) {
+        output[i] = input0[i] * input1[i];
+      }
+      #else
+      for (int i = startIndex; i < endIndex; i++) {
+        output[i] = input0[i] * input1[i];
+      }
+      #endif
+    }
+  
+    static inline void multiply(float *input, float constant, float *output, int startIndex, int endIndex) {
+      #ifdef _ARM_ARCH_7
+      const int numFours = (endIndex - startIndex) >> 2;
+      for (int i = startIndex, j = 0; j < numFours; i+=4, j++) {
+        float32x4_t inVec = vld1q_f32((const float32_t *) (input + i));
+        float32x4_t res = vmulq_n_f32(inVec, constant);
+        vst1q_f32((float32_t *) (output + i), res);
+      }
+      for (int i = startIndex + numFours<<2; i < endIndex; i++) {
+        output[i] = input[i] * constant;
+      }
+      #else
+      for (int i = startIndex; i < endIndex; i++) {
+        output[i] = input[i] * constant;
+      }
+      #endif
+    }
+    
+    // recipocal: vrecpeq_f32
+    static inline void divide(float *a, float *b, int startIndex, int endIndex);
+    static inline void divide(float *a, float b, int startIndex, int endIndex);
+    
+  private:
+    ArrayArithmetic(); // no instances of this object are allowed
+    ~ArrayArithmetic();
+};
+
+#endif // _ARRAY_ARITHMETIC_H_
--- src/DspMultiply.cpp
+++ src/DspMultiply.cpp
@@ -20,12 +20,9 @@
  *
  */
 
+#include "ArrayArithmetic.h"
 #include "DspMultiply.h"
 #include "PdGraph.h"
-
-#ifdef _ARM_ARCH_7
-#include <arm_neon.h>
-#endif // _ARM_ARCH_7
 
 DspMultiply::DspMultiply(PdMessage *initMessage, PdGraph *graph) : DspObject(2, 2, 0, 1, graph) {
   constant = initMessage->isFloat(0) ? initMessage->getFloat(0) : 0.0f;
@@ -58,30 +55,8 @@ void DspMultiply::processMessage(int inletIndex, PdMessage *message) {
 void DspMultiply::processDspToIndex(float blockIndex) {
   switch (signalPrecedence) {
     case DSP_DSP: {
-      const int startSampleIndex = getStartSampleIndex();
-      const int endSampleIndex = getEndSampleIndex(blockIndex);
-      const float *inputBuffer0 = localDspBufferAtInlet[0];
-      const float *inputBuffer1 = localDspBufferAtInlet[1];
-      float *outputBuffer = localDspBufferAtOutlet[0];
-      
-      #ifdef _ARM_ARCH_7
-      // the number of sets of four samples in the block to be processed
-      const int numFours = (endSampleIndex - startSampleIndex) >> 2;
-      for (int i = startSampleIndex, j = 0; j < numFours; i+=4, j++) {
-        float32x4_t inVec0 = vld1q_f32((const float32_t *) (inputBuffer0 + i)); // use VLD1 as data is NOT interleaved
-        float32x4_t inVec1 = vld1q_f32((const float32_t *) (inputBuffer1 + i)); // load
-        float32x4_t res = vmulq_f32(inVec0, inVec1); // compute
-        vst1q_f32((float32_t *) (outputBuffer + i), res); // store
-      }
-      // compute the remainder of the block (if any)
-      for (int i = startSampleIndex + numFours<<2; i < endSampleIndex; i++) {
-        outputBuffer[i] = inputBuffer0[i] * inputBuffer1[i];
-      }
-      #else
-      for (int i = startSampleIndex; i < endSampleIndex; i++) {
-        outputBuffer[i] = inputBuffer0[i] * inputBuffer1[i];
-      }
-      #endif
+      ArrayArithmetic::multiply(localDspBufferAtInlet[0], localDspBufferAtInlet[1], 
+          localDspBufferAtOutlet[0], getStartSampleIndex(), getEndSampleIndex(blockIndex));
       
       /*
        * This approach is cleaner, considering that in DSP_DSP mode, no message should be received
@@ -108,26 +83,8 @@ void DspMultiply::processDspToIndex(float blockIndex) {
       break;
     }
     case DSP_MESSAGE: {
-      const int startSampleIndex = getStartSampleIndex();
-      const int endSampleIndex = getEndSampleIndex(blockIndex);
-      const float *inputBuffer = localDspBufferAtInlet[0];
-      float *outputBuffer = localDspBufferAtOutlet[0];
-
-      #ifdef _ARM_ARCH_7
-      const int numFours = (endSampleIndex - startSampleIndex) >> 2;
-      for (int i = startSampleIndex, j = 0; j < numFours; i+=4, j++) {
-        float32x4_t inVec = vld1q_f32((const float32_t *) (inputBuffer + i));
-        float32x4_t res = vmulq_n_f32(inVec, constant);
-        vst1q_f32((float32_t *) (outputBuffer + i), res);
-      }
-      for (int i = startSampleIndex + numFours<<2; i < endSampleIndex; i++) {
-        outputBuffer[i] = inputBuffer[i] * constant;
-      }
-      #else
-      for (int i = startSampleIndex; i < endSampleIndex; i++) {
-        outputBuffer[i] = inputBuffer[i] * constant;
-      }
-      #endif
+      ArrayArithmetic::multiply(localDspBufferAtInlet[0], constant, localDspBufferAtOutlet[0],
+          getStartSampleIndex(), getEndSampleIndex(blockIndex));
       break;
     }
     case MESSAGE_DSP:
