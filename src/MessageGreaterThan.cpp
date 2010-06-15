8ce3e14372317954451898a108ee3b7310fa3862
Yoonchang Han <yoonchang@yoonchang-laptop.(none)>
2010-06-15 14:44:49 +0100

    added test for MessageGreaterThan and updated object to conform to test

--- src/MessageGreaterThan.cpp
+++ src/MessageGreaterThan.cpp
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2009, 2010 Reality Jockey, Ltd.
+ *  Copyright 2009,2010 Reality Jockey, Ltd.
  *                 info@rjdj.me
  *                 http://rjdj.me/
  *
@@ -23,46 +23,42 @@
 #include "MessageGreaterThan.h"
 
 MessageGreaterThan::MessageGreaterThan(PdMessage *initMessage, PdGraph *graph) : MessageObject(2, 1, graph) {
-  if (initMessage->getNumElements() > 0 &&
-      initMessage->getElement(0)->getType() == FLOAT) {
-    init(initMessage->getElement(0)->getFloat());
-  } else {
-    init(0.0f);
-  }
-}
-
-MessageGreaterThan::MessageGreaterThan(float constant, PdGraph *graph) : MessageObject(2, 1, graph) {
-  init(constant);
+  constant = initMessage->isFloat(0) ? initMessage->getFloat(0) : 0.0f;
+  lastOutput = 0.0f;
 }
 
 MessageGreaterThan::~MessageGreaterThan() {
   // nothing to do
 }
 
-void MessageGreaterThan::init(float constant) {
-  this->constant = constant;
-}
-
 const char *MessageGreaterThan::getObjectLabel() {
-  return ">";
+  return "!=";
 }
 
 void MessageGreaterThan::processMessage(int inletIndex, PdMessage *message) {
   switch (inletIndex) {
     case 0: {
-      MessageElement *messageElement = message->getElement(0);
-      if (messageElement->getType() == FLOAT) {
-        PdMessage *outgoingMessage = getNextOutgoingMessage(0);
-        outgoingMessage->getElement(0)->setFloat((messageElement->getFloat() > constant) ? 1.0f : 0.0f);
-        outgoingMessage->setTimestamp(message->getTimestamp());
-        sendMessage(0, outgoingMessage); // send a message from outlet 0
+      switch (message->getType(0)) {
+        case FLOAT: {
+          lastOutput = (message->getFloat(0) > constant) ? 1.0f : 0.0f;
+          // allow fallthrough
+        }
+        case BANG: {
+          PdMessage *outgoingMessage = getNextOutgoingMessage(0);
+          outgoingMessage->setFloat(0, lastOutput);
+          outgoingMessage->setTimestamp(message->getTimestamp());
+          sendMessage(0, outgoingMessage);
+          break;
+        }
+        default: {
+          break;
+        }
       }
       break;
     }
     case 1: {
-      MessageElement *messageElement = message->getElement(0);
-      if (messageElement->getType() == FLOAT) {
-        constant = messageElement->getFloat();
+      if (message->isFloat(0)) {
+        constant = message->getFloat(0);
       }
       break;
     }
