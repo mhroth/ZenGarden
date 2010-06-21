7e18937c31585795c480aab60f2c761a2b6a25bf
Martin Roth <mhroth@rjdj.me>
2010-06-21 01:23:36 +0100

    Update vd~ to check for availability of Accelerate framework.

--- src/DspVariableDelay.cpp
+++ src/DspVariableDelay.cpp
@@ -57,51 +57,53 @@ void DspVariableDelay::processDspToIndex(float newBlockIndex) {
   float *outputBuffer = localDspBufferAtOutlet[0];
   
   float targetIndexBase = (float) (headIndex - blockSizeInt);
-  #if TARGET_OS_MAC || TARGET_OS_IPHONE
-  // calculate delay in samples (vector version of StaticUtils::millisecondsToSamples)
-  float samplesPerMillisecond = sampleRate / 1000.0f;
-  vDSP_vsmul(inputBuffer, 1, &samplesPerMillisecond, xArray, 1, blockSizeInt);
-  
-  float zero = 0.0f;
-  float one = 1.0f;
-  // TODO(mhroth): because vDSP_vtabi already does clipping to the buffer bounds, can the following
-  // steps be rearranged or optimised in order to provide the minimum necessary information to the
-  // tablel lookup. Let it do as much work as possible.
-  vDSP_vclip(xArray, 1, &zero, &bufferLengthFloat, xArray, 1, blockSizeInt); // clip the delay between 0 and the buffer length
-  vDSP_vramp(&targetIndexBase, &one, targetIndexBaseArray, 1, blockSizeInt);  // create targetIndexBaseArray
-  vDSP_vsub(xArray, 1, targetIndexBaseArray, 1, xArray, 1, blockSizeInt); // targetIndexBaseArray - xArray (== targetSampleIndex)
-  
-  // ensure that targetSampleIndex is positive
-  // TODO(mhroth): vectorise this!
-  for (int i = 0; i < blockSizeInt; i++) {
-    if (xArray[i] < 0.0f) {
-      xArray[i] += bufferLengthFloat;
-    }
-  }
-
-  // do table lookup (in buffer) using xArray as indicies, with linear interpolation 
-  vDSP_vtabi(xArray, 1, &one, &zero, buffer, bufferLength+1, outputBuffer, 1, blockSizeInt);
-  #else
-  for (int i = 0; i < blockSizeInt; i++, targetIndexBase+=1.0f) {
-    float delayInSamples = StaticUtils::millisecondsToSamples(inputBuffer[i], sampleRate);
-    if (delayInSamples < 0.0f) {
-      delayInSamples = 0.0f;
-    } else if (delayInSamples > bufferLengthFloat) {
-      delayInSamples = bufferLengthFloat;
+  if (ArrayArithmetic::hasAccelerate) {
+    #if __APPLE__
+    // calculate delay in samples (vector version of StaticUtils::millisecondsToSamples)
+    float samplesPerMillisecond = sampleRate / 1000.0f;
+    vDSP_vsmul(inputBuffer, 1, &samplesPerMillisecond, xArray, 1, blockSizeInt);
+    
+    float zero = 0.0f;
+    float one = 1.0f;
+    // TODO(mhroth): because vDSP_vtabi already does clipping to the buffer bounds, can the following
+    // steps be rearranged or optimised in order to provide the minimum necessary information to the
+    // tablel lookup. Let it do as much work as possible.
+    vDSP_vclip(xArray, 1, &zero, &bufferLengthFloat, xArray, 1, blockSizeInt); // clip the delay between 0 and the buffer length
+    vDSP_vramp(&targetIndexBase, &one, targetIndexBaseArray, 1, blockSizeInt);  // create targetIndexBaseArray
+    vDSP_vsub(xArray, 1, targetIndexBaseArray, 1, xArray, 1, blockSizeInt); // targetIndexBaseArray - xArray (== targetSampleIndex)
+    
+    // ensure that targetSampleIndex is positive
+    // TODO(mhroth): vectorise this!
+    for (int i = 0; i < blockSizeInt; i++) {
+      if (xArray[i] < 0.0f) {
+        xArray[i] += bufferLengthFloat;
+      }
     }
     
-    float targetSampleIndex = targetIndexBase - delayInSamples;
-    if (targetSampleIndex < 0.0f) {
-      targetSampleIndex += bufferLengthFloat;
+    // do table lookup (in buffer) using xArray as indicies, with linear interpolation 
+    vDSP_vtabi(xArray, 1, &one, &zero, buffer, bufferLength+1, outputBuffer, 1, blockSizeInt);
+    #endif
+  } else {
+    for (int i = 0; i < blockSizeInt; i++, targetIndexBase+=1.0f) {
+      float delayInSamples = StaticUtils::millisecondsToSamples(inputBuffer[i], sampleRate);
+      if (delayInSamples < 0.0f) {
+        delayInSamples = 0.0f;
+      } else if (delayInSamples > bufferLengthFloat) {
+        delayInSamples = bufferLengthFloat;
+      }
+      
+      float targetSampleIndex = targetIndexBase - delayInSamples;
+      if (targetSampleIndex < 0.0f) {
+        targetSampleIndex += bufferLengthFloat;
+      }
+      
+      // 2-point linear interpolation (basic and fast)
+      int x0 = (int) targetSampleIndex;
+      float dx = targetSampleIndex - ((float) x0);
+      float y0 = buffer[x0];
+      float y1 = buffer[x0+1];
+      float slope = (y1 - y0); // /(x1 - x0) == 1.0f!
+      outputBuffer[i] = (slope * dx) + y0;
     }
-    
-    // 2-point linear interpolation (basic and fast)
-    int x0 = (int) targetSampleIndex;
-    float dx = targetSampleIndex - ((float) x0);
-    float y0 = buffer[x0];
-    float y1 = buffer[x0+1];
-    float slope = (y1 - y0); // /(x1 - x0) == 1.0f!
-    outputBuffer[i] = (slope * dx) + y0;
   }
-  #endif
 }
