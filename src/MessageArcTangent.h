4095445fa824ae0fa11645729bc1dfade3d18476
Martin Roth <mhroth@rjdj.me>
2010-06-23 16:32:24 +0100

    Tidy MessageArcTangent.

--- src/MessageArcTangent.h
+++ src/MessageArcTangent.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2009, 2010 Reality Jockey, Ltd.
+ *  Copyright 2009,2010 Reality Jockey, Ltd.
  *                 info@rjdj.me
  *                 http://rjdj.me/
  *
@@ -31,7 +31,6 @@ class MessageArcTangent : public MessageObject {
 
   public:
     MessageArcTangent(PdMessage *initMessage, PdGraph *graph);
-    MessageArcTangent(PdGraph *graph);
     ~MessageArcTangent();
 
     const char *getObjectLabel();
