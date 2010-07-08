69d987275353329b1073273081006b9a6e4e66d9
Yoonchang Han <yoonchang@yoonchang-laptop.(none)>
2010-07-08 19:47:03 +0100

    Add fix to MessageRandom in order to pass the test

--- src/MessageRandom.cpp
+++ src/MessageRandom.cpp
@@ -2,7 +2,7 @@
  *  Copyright 2009 Reality Jockey, Ltd.
  *                 info@rjdj.me
  *                 http://rjdj.me/
- * 
+ *
  *  This file is part of ZenGarden.
  *
  *  ZenGarden is free software: you can redistribute it and/or modify
@@ -14,7 +14,7 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with ZenGarden.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -45,6 +45,10 @@ const char *MessageRandom::getObjectLabel() {
   return "random";
 }
 
+bool MessageRandom::shouldDistributeMessageToInlets() {
+  return false;
+}
+
 void MessageRandom::processMessage(int inletIndex, PdMessage *message) {
   switch (inletIndex) {
     case 0: {
@@ -69,6 +73,7 @@ void MessageRandom::processMessage(int inletIndex, PdMessage *message) {
           break;
         }
       }
+      break;
     }
     case 1: {
       if (message->getElement(0)->getType() == FLOAT) {
