de49b6dbe13f65dd8477010be8b728a4945b05fa
Martin Roth <mhroth@Martin-Roths-MacBook.local>
2011-05-25 16:57:21 +0100

    Fix error in MessagePow in which original message was mistakenly being changed, not outgoing message.

--- src/MessagePow.cpp
+++ src/MessagePow.cpp
@@ -40,7 +40,7 @@ void MessagePow::processMessage(int inletIndex, PdMessage *message) {
       if (message->isFloat(0)) {
         PdMessage *outgoingMessage = PD_MESSAGE_ON_STACK(1);
         float value = (message->getFloat(0) <= 0.0f) ? 0.0f : powf(message->getFloat(0), constant);
-        message->initWithTimestampAndFloat(message->getTimestamp(), value);
+        outgoingMessage->initWithTimestampAndFloat(message->getTimestamp(), value);
         sendMessage(0, outgoingMessage);
       }
       break;
