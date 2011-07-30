a76291fab8a94a4b45c630a1c7bf57a4d3b2c275
Martin Roth <mhroth@gmail.com>
2011-07-30 03:33:47 +0100

    Update test script to run all tests.

--- runme-test.sh
+++ runme-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-java -Djava.library.path=./libs/`./src/platform`/ -classpath ./ZenGarden.jar:./junit-4.8.2.jar org.junit.runner.JUnitCore me.rjdj.zengarden.PdObjectTest
+java -Djava.library.path=./libs/`./src/platform`/ -classpath ./ZenGarden.jar:./junit-4.8.2.jar org.junit.runner.JUnitCore me.rjdj.zengarden.PdObjectTest me.rjdj.zengarden.ZGSystemTest
