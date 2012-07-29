03cc1f8a885f0caffbdf5680949285bd658c3ff4
Martin Roth <mhroth@gmail.com>
2012-07-29 18:08:42 +0200

    First commit of vline~

---
+++ Xcode/DspVariableLine.h
@@ -0,0 +1,14 @@
+//
+//  DspVariableLine.h
+//  ZenGarden
+//
+//  Created by Martin Roth on 29/07/2012.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+
+#ifndef ZenGarden_DspVariableLine_h
+#define ZenGarden_DspVariableLine_h
+
+
+
+#endif
---
+++ src/DspVariableLine.cpp
@@ -0,0 +1,9 @@
+//
+//  DspVariableLine.cpp
+//  ZenGarden
+//
+//  Created by Martin Roth on 29/07/2012.
+//  Copyright (c) 2012 -. All rights reserved.
+//
+
+#include <iostream>
